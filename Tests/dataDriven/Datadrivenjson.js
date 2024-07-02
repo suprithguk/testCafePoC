b31b4194f3a0719a4bd2a23b37bc122b5baf532f
suprithguk <suprithguk@gmail.com>
2024-07-02 16:40:33 +0100

    Update Datadrivenjson.js

--- Tests/dataDriven/Datadrivenjson.js
+++ Tests/dataDriven/Datadrivenjson.js
@@ -1,2 +1,16 @@
 import {Selector, t, ClientFunction} from 'testcafe';
 const dataSet = require('./data/dataJsonFile.json')
+
+fixture("Data driven testing with json files")
+dataSet.forEach(data => {
+  console.log(data)
+  test.page("url")
+  ("Login test - $expectedMessage", async t => {
+    await t
+    .typeText(Selector('input#username'), data.username)
+    .typeText(Selector('input#password'), data.password)
+    .click('button')
+
+    t.expect(Selector('div#flash').innerText, contains data.expectedMessage);
+  })
+});
